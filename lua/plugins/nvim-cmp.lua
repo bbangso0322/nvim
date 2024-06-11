04ddb9a5fe671bbd63da604335b4a6ac64693613
bbangso0322 <bbangso0322@gmail.com>
2024-06-11 23:27:52 +0900

    add nvim-cmp (TODO: fix key mapping error)

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,40 @@
+return {
+    "hrsh7th/nvim-cmp",
+    version = false,
+    event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+    },
+
+    config = function()
+        local cmp = require("cmp")
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    -- add snippet engine you need
+                    -- example:  
+                    -- luasnip.lsp_expand(args.body)
+                end
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select`
+            }),
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "path", max_item_count = 3},
+            }, {
+                { name = "buffer", max_item_count = 3 },
+            })
+        })
+    end
+}
