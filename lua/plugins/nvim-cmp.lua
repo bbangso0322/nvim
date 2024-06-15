fbeb0f32d11d44b18abfff71ac1b11158932d223
bbangso0322 <bbangso0322@gmail.com>
2024-06-16 00:43:23 +0900

    add nvim-cmp plugin

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,20 +1,24 @@
 return {
     "hrsh7th/nvim-cmp",
     version = false,
-    event = "InsertEnter",
+    event = { "BufReadPost", "BufNewFile"},
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
     },
+    
 
     config = function()
         local cmp = require("cmp")
+        vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+        print("cmp called!")
         cmp.setup({
             snippet = {
                 expand = function(args)
                     -- add snippet engine you need
-                    -- example:  
+                    -- example:
                     -- luasnip.lsp_expand(args.body)
                 end
             },
@@ -23,11 +27,25 @@ return {
                 documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select`
+                ['<Tab>'] = function(fallback)
+                    if cmp.visible() then
+                    cmp.select_next_item()
+                    else
+                    fallback()
+                    end
+                end,
+                ['<S-Tab>'] = function(fallback)
+                    if cmp.visible() then
+                    cmp.select_prev_item()
+                    else
+                    fallback()
+                    end
+                end,
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<Esc>'] = cmp.mapping.close(),
+                ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
             }),
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
