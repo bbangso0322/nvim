2921dbaa2141f0a8eb9ec56b6da9f17c200d8358
bbangso0322 <bbangso0322@gmail.com>
2026-01-22 00:26:09 +0900

    update neo-tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,57 +2,48 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
+    lazy = false,
+    init = function()
+        vim.diagnostic.config({
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = '',
+                    [vim.diagnostic.severity.WARN]  = '',
+                    [vim.diagnostic.severity.INFO]  = '',
+                    [vim.diagnostic.severity.HINT]  = '󰌵',
+                },
+            }
+        })
+
+
+    -- git_status = {
+    --   symbols = {
+    --     -- Change type
+    --     added = "", -- or "✚"
+    --     modified = "", -- or ""
+    --     deleted = "✖", -- this can only be used in the git_status source
+    --     renamed = "󰁕", -- this can only be used in the git_status source
+    --     -- Status type
+    --     untracked = "",
+    --     ignored = "",
+    --     unstaged = "󰄱",
+    --     staged = "",
+    --     conflict = "",
+    --   },
+    -- },
+    end,
     config = function()
-        vim.fn.sign_define("DiagnosticSignError",
-            {text = " ", texthl = "DiagnosticSignError"})
-        vim.fn.sign_define("DiagnosticSignWarn",
-            {text = " ", texthl = "DiagnosticSignWarn"})
-        vim.fn.sign_define("DiagnosticSignInfo",
-            {text = " ", texthl = "DiagnosticSignInfo"})
-        vim.fn.sign_define("DiagnosticSignHint",
-            {text = "󰌵", texthl = "DiagnosticSignHint"})
-
-        local os_name = vim.loop.os_uname().sysname
-        local execute_cmd = "start"
-        
-        if os_name ~= "Windows_NT" then
-            execute_cmd = "open"
-        end
-
         require("neo-tree").setup({
-            window = {
-                mappings = {
-                    ["x"] = {
-                        function(state)
-                            local node = state.tree:get_node()
-                            local path = node:get_id()
-                            vim.api.nvim_command("silent !" .. execute_cmd .. " " .. path)
-                        end
-                    }
+            filesystem = {
+                filtered_items = {
+                    hide_dotfiles = false,
+                    hide_hidden = false,
                 }
             }
         })
     end,
-    cmd = "Neotree",
-    keys = {
-        {
-            "<leader>e",
-            function()
-                require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
-            end,
-            desc = "Open neotree cwd"
-        },
-        {
-            "<leader>ge",
-            function()
-                require("neo-tree.command").execute({ source = "git_status", toggle = true })
-            end,
-            desc = "Git Explorer",
-        },
-    }
 }
