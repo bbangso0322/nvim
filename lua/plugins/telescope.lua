0f1664261fac132e5021e083100d3fac21d2d1f9
bbangso0322 <bbangso0322@gmail.com>
2026-03-20 15:36:10 +0900

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,4 +3,11 @@ return {
     dependencies = {
         'nvim-lua/plenary.nvim',
     },
+    keys = {
+        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+        { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+        { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+        { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+        { "<leader>fw", "<cmd>Telescope grep_string<cr>", desc = "Grep for word"},
+    },
 }
